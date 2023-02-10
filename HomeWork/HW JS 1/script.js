9ac34b22e02187f62380a63be10a98faae8de8f9
LevonPetrosyan2002 <levon.petrosyan17@mail.ru>
2023-02-10 14:26:16 +0400

    add folder lesson and home work

---
+++ HomeWork/HW JS 1/script.js
@@ -0,0 +1,4 @@
+// let x=36
+// let y=6
+// x%y
+// console.log(x%y)
---
+++ HomeWork/HW JS 2/script.js
@@ -0,0 +1,35 @@
+// let n=parseInt(prompt("naxnakan tiv"))
+// let k=parseInt(prompt("verjnakan tiv"))
+// for(n;n<k;n++){
+//    res=n+n+1
+//
+// }
+// console.log(res)
+
+// let n=parseInt(prompt("naxnakan tiv"))
+// let k=parseInt(prompt("verjnakan tiv"))
+// for(n;n<k;n++){
+//     if(n%2==0){
+//         res=n+n+1
+//     }
+//
+//
+// }
+// console.log(res)
+
+
+
+
+
+// let n=parseInt(prompt("naxnakan tiv"))
+// let k=parseInt(prompt("verjnakan tiv"))
+// for(let i=n;i<k;i++){
+//     let t=i-1
+//     let gumar=t+i
+//     if(gumar%3==0){
+//         console.log(gumar)
+//     }
+//
+//
+//
+// }
